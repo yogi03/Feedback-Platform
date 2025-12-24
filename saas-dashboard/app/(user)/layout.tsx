b8a4a1e6945fa0989277d0c90d0578e7627c698d
yogi03 <yogendrachaurasiya30@gmail.com>
2025-12-25 00:52:28 +0530

    Feedbacks table

--- saas-dashboard/app/(user)/layout.tsx
+++ saas-dashboard/app/(user)/layout.tsx
@@ -3,7 +3,7 @@ import { Suspense } from "react"
 
 export default function UserLayout({children} : {children: React.ReactNode}) {
     return (
-       <div className="container w-full max-w-screen-xl mx-auto py-10">
+       <div className="container w-full max-w-7xl mx-auto py-10">
         <Suspense fallback={<Loading />}>
             {children}
         </Suspense>
